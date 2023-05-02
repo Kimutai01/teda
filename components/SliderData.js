1587c905579c3bdcec25ac901c277f78ef763928
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-05-02 08:07:36 +0300

    Updated images

--- components/SliderData.js
+++ components/SliderData.js
@@ -1,25 +1,25 @@
 export const SliderData = [
   {
     image:
-      "https://images.unsplash.com/photo-1567596045677-50ea5cdf2ea7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMHRyYWRpdGlvbnN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      "https://res.cloudinary.com/dapnnry4b/image/upload/v1682966395/slider_3_lnioqf.jpg",
     title: "Teda NGO",
     description: "We are a non-profit organization that helps people in need.",
   },
   {
     image:
-      "https://images.unsplash.com/photo-1599819548353-2c32ed28cd6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGN1bHR1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      "https://res.cloudinary.com/dapnnry4b/image/upload/v1682966438/1682588950093_163_1_j2z3qs.jpg",
     title: "Teda NGO",
     description: "We are a non-profit organization that helps people in need.",
   },
   {
     image:
-      "https://images.unsplash.com/photo-1604095087270-be6e0ea58fb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YWZyaWNhbiUyMGFydHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+      "https://res.cloudinary.com/dapnnry4b/image/upload/v1682966395/slider_3_lnioqf.jpg",
     title: "Teda NGO",
     description: "We are a non-profit organization that helps people in need.",
   },
   {
     image:
-      "https://images.unsplash.com/photo-1519635451045-a41d4361d495?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGFmcmljYW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      "https://res.cloudinary.com/dapnnry4b/image/upload/v1682966396/slider_2_hxwmyl.jpg",
     title: "Teda NGO",
     description: "We are a non-profit organization that helps people in need.",
   },
